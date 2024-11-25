6b7ba0548b3af953a262c6e0b22b736606406cce
a23luciacv <a23luciacv@iessanclemente.net>
2024-11-25 12:00:45 +0100

    añadir navegación

--- ArtSpace/app/src/main/java/com/example/artspace/MainMenuFragment.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainMenuFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.artspace.adapter.MainMenuAdapter
 import com.example.artspace.databinding.FragmentMainMenuBinding
 import com.example.artspace.data.MainMenuData
+import com.example.artspace.model.MenuItem
 
 class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
 
@@ -25,10 +27,31 @@ class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
        val dataset = MainMenuData().LoadMenuItem()
         val recyclerView = binding.mainRecycler
 
-        recyclerView.adapter = MainMenuAdapter(this, dataset)
+        //recyclerView.adapter = MainMenuAdapter(this, dataset)
+
+        //AÑADIDO
+        recyclerView.adapter = MainMenuAdapter(this, dataset) { item ->
+            // Aquí manejas el clic en el ítem, según el id o el tipo de ítem
+            onMenuItemClicked(item)
+        }
+
         recyclerView.setHasFixedSize(true)
 
         return binding.root
+    }
+
+    private fun onMenuItemClicked(item: MenuItem) {
+        when (item.nameRes) {
+            R.string.online_gallery -> {
+                // Navegar a la opción 1
+                findNavController().navigate(R.id.action_mainMenuFragment2_to_secondaryMenuFragment)
+            }
+            R.string.your_gallery -> {
+                // Navegar a la opción 2
+                findNavController().navigate(R.id.action_mainMenuFragment2_to_galleryFragment)
+            }
+            // Agregar más casos si es necesario
+        }
     }
 
     override fun onDestroyView() {
--- ArtSpace/app/src/main/java/com/example/artspace/adapter/MainMenuAdapter.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/adapter/MainMenuAdapter.kt
@@ -14,6 +14,7 @@ import com.example.artspace.model.MenuItem
 class MainMenuAdapter(
     private val context: MainMenuFragment,
     private val dataset: List<MenuItem>,
+    private val onItemClick: (MenuItem) -> Unit // AÑADIDO
 ) : RecyclerView.Adapter<MainMenuAdapter.ItemViewHolder>() {
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.menuItemText)
@@ -43,6 +44,10 @@ class MainMenuAdapter(
         } else {
             // Alinear el texto a la derecha para ítems en posiciones impares
             holder.textView.gravity = Gravity.END
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClick(view)  // Llamar a la función onItemClick que recibimos como parámetro
         }
     }
 }
